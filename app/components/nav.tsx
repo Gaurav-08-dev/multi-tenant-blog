2b0c0814433e9abccfcb3e484e9c8c89a1b17a4d
gaurav <g8111997@gmail.com>
2025-06-24 01:01:47 +0530

    Setup project

---
+++ app/components/nav.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+const Nav = () => {
+    return (
+        <nav className="bg-gray-800 p-4">
+            <div>
+            <h1 className="text-white text-lg">My Blog</h1>
+            </div>
+            <div></div>
+        </nav>
+    );
+}
+
+export default Nav;
